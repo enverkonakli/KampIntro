395e3c2f5b5c33d424c0bb25b3c4609e685495d9
enverkonakli <enver.konakli1052@gmail.com>
2023-09-01 00:13:07 +0300

    kampIntro

---
+++ Collections/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = { "Enver", "Murat", "Kenan", "Selen" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+
+            Console.WriteLine("----------------------------------------------");
+            List<string> isimler2 = new List<string>(); 
+            foreach (string s in isimler) 
+            {
+                isimler2.Add(s);
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace OOP1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitInStock = 10;
+
+            Product product2 = new Product
+            {
+                Id = 2,
+                CategoryId = 3,
+                ProductName = "Kalem",
+                UnitPrice = 200,
+                UnitInStock = 5,
+            };
+
+
+            ProductManager productManager = new ProductManager();
+        }
+    }
+}
